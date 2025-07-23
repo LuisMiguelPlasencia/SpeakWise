6fd96440d0abb724945b6b1674cf351b86896a82
Mike <lmpc1504@gmail.com>
2025-07-23 17:54:41 +0200

    Change Port

--- server/index.ts
+++ server/index.ts
@@ -60,7 +60,7 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = Number(process.env.PORT) || '5000';
+  const port = Number(process.env.PORT) || '8080';
   server.listen(port, () => {
     log(`serving on port ${port}`);
   });
